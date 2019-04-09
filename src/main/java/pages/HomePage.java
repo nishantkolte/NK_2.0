f107089bf221c791663ee3eea3fdba5492818c20
NISH <NISH@LAPTOP-G0ITPRBD>
2019-04-10 01:21:12 +0530

    Homepage commit

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.DriverInit;
+
+public class HomePage {
+	static WebDriver driver=DriverInit.driver;
+	public HomePage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	@FindBy (xpath="//a[text()=\"Home\"]")
+	public static WebElement HomeTab;
+
+	@FindBy (xpath="//a[contains(text(),\"Login Management\")]")
+	public static WebElement LoginManagementLink;
+	
+}
